582c90ae84bbf4f8f53eaa28e6489e6b82218d20
EDEM <edemur19@gmail.com>
2022-01-25 19:29:05 +0600

    first modification

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function countNumbers(
+    $first, 
+    $second,
+    $third
+)
+
+{
+    echo
+    "$first is the first nuber "<br>
+    "$second after "<br>
+    "$third is a finish number "<br>
+
+}
+
+countNumbers(1,2,3)
+
